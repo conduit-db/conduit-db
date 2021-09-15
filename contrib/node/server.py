1e1cb10582076686432c3f49c164a94292247d7a
Roger Taylor <roger.taylor.email@gmail.com>
2021-09-15 19:14:05 +1200

    Get the RPC port properly exposed to the host. It works for P2P as P2P binds to 0.0.0.0 by default, but RPC has to be explicitly told to do so. This is done in `contrib\node\server.py` where any other server options can be specified.

--- contrib/node/server.py
+++ contrib/node/server.py
@@ -3,10 +3,11 @@ import subprocess
 
 from electrumsv_node import electrumsv_node
 
-
 extra_config_options = [
-    f"-debug=1",
-    f"-rejectmempoolrequest=0"
+    "-debug=1",
+    "-rejectmempoolrequest=0",
+    "-rpcallowip=0.0.0.0/0",
+    "-rpcbind=0.0.0.0",
 ]
 split_command = electrumsv_node.shell_command(print_to_console=True, extra_params=extra_config_options)
 process = subprocess.Popen(" ".join(split_command), shell=True, env=os.environ.copy())
