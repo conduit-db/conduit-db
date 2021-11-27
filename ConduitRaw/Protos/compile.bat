df0cfa69e685fd04e3df2577072b0f6e69e14f25
AustEcon <AustEcon0922@gmail.com>
2021-11-27 16:26:28 +1300

    Delete ConduitRaw C# partial implementation (preserved in conduit-db-sharp forked repository)

--- ConduitRaw/Protos/compile.bat
+++
@@ -1,9 +0,0 @@
-REM protoc --csharp_out=. .\greet.proto
-py -m grpc_tools.protoc --proto_path=. --python_out=. --mypy_out=. --grpc_python_out=. .\conduit_raw.proto
-copy .\conduit_raw.proto ..\..\conduit_lib\.\conduit_raw.proto
-copy conduit_raw_pb2.py ..\..\conduit_lib\conduit_raw_pb2.py
-copy conduit_raw_pb2_grpc.py ..\..\conduit_lib\conduit_raw_pb2_grpc.py
-copy conduit_raw_pb2.pyi ..\..\conduit_lib\conduit_raw_pb2.pyi 
-del conduit_raw_pb2.py
-del conduit_raw_pb2_grpc.py
-del conduit_raw_pb2.pyi
