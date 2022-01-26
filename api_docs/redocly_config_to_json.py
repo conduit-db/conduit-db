59ac7462efa8514d4cf729d324478c4dbe184235
AustEcon <AustEcon0922@gmail.com>
2022-01-26 16:59:51 +1300

    Add ReDoc and made a script for overwriting <script> section (#22)
    with customstyles.css at will.
    - Produces a standalone html file for all of the documentation.

---
+++ api_docs/redocly_config_to_json.py
@@ -0,0 +1,14 @@
+import ruamel.yaml
+import json
+
+in_file = '.redocly.yaml'
+out_file = '.redocly.json'
+
+yaml = ruamel.yaml.YAML(typ='safe')
+with open(in_file) as fi:
+    yaml_object = yaml.load(fi)
+with open(out_file, 'w') as fo:
+    json.dump(yaml_object['referenceDocs'], fo, separators=(",", ":"))
+
+
+
