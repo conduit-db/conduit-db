2e5565e5d87ee23bba7f04ee55a6273c885107d9
AustEcon <AustEcon0922@gmail.com>
2022-06-25 16:36:34 +1200

    Fix unittests after removing the default datadir concept
    - Must always set database locations in environment variables now to avoid any confusion

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,10 @@ from pathlib import Path
 from typing import Callable, Optional
 
 os.environ['NETWORK'] = 'regtest'
-
+os.environ['LMDB_DATABASE_DIR'] = './lmdb_test'
+os.environ['RAW_BLOCKS_DIR'] = './raw_blocks'
+os.environ['MERKLE_TREES_DIR'] = './merkle_trees'
+os.environ['TX_OFFSETS_DIR'] = './tx_offsets'
 
 def remove_readonly(func: Callable[[Path], None], path: Path,
         excinfo: Optional[BaseException]) -> None:
